d82f2a909733a77c0be46769afa328c023f2c4ab
Eleonora Mattera <m.eleonora98@live.it>
2023-01-24 15:15:42 +0100

    js

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ const rigaUno = document.getElementById("uno");
 const rigaDue = document.getElementById("due");
 const rigaTre = document.getElementById("tre");
 
-let Person = function () {
-  this.name = " ";
-  this.surname = " ";
-  this.dataNascita = " ";
+let Person = function (name, surname, dataNascita) {
+  this.name = name;
+  this.surname = surname;
+  this.dataNascita = dataNascita;
 };
 
-let user1 = new Person();
-console.log(user1);
+let user1 = new Person(name, surname, dataNascita);
+
+bottone.onclick = function () {
+  user1.name = name.value;
+  user1.surname = surname.value;
+  user1.dataNascita = dataNascita.value;
+
+  rigaUno.innerHTML = user1.name;
+  rigaDue.innerHTML = user1.surname;
+  rigaTre.innerHTML = user1.dataNascita;
+};
